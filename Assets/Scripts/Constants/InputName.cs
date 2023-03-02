860ae7f22ed8084b90e6197e5c0436af81050a58
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-03-02 19:24:04 +0900

    インプット名の定数を管理するスクリプトを作成した

---
+++ Assets/Scripts/Constants/InputName.cs
@@ -0,0 +1,24 @@
+﻿namespace Template.Constant
+{
+	/// <summary>
+	/// インプット名を定数で管理するクラス
+	/// </summary>
+	public struct InputName
+	{
+		#region Constants
+	
+		  public const string HORIZONTAL = "Horizontal";
+		  public const string VERTICAL = "Vertical";
+		  public const string FIRE1 = "Fire1";
+		  public const string FIRE2 = "Fire2";
+		  public const string FIRE3 = "Fire3";
+		  public const string JUMP = "Jump";
+		  public const string MOUSE_X = "Mouse X";
+		  public const string MOUSE_Y = "Mouse Y";
+		  public const string MOUSE_SCROLLWHEEL = "Mouse ScrollWheel";
+		  public const string SUBMIT = "Submit";
+		  public const string CANCEL = "Cancel";
+	
+		#endregion
+	}
+}
