ee75bb76c89a4e93ba5ccce4df8aaebd234bdaeb
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-03-09 17:19:18 +0900

    エディター拡張したいスクリプトを作成するために必要なものを作った

---
+++ Assets/Scripts/Constants/RootNameSpaceName.cs
@@ -0,0 +1,14 @@
+﻿namespace Template.Constant
+{
+	/// <summary>
+	/// 名前空間名を定数で管理するクラス
+	/// </summary>
+	public struct RootNameSpaceName
+	{
+		#region Constants
+	
+		  public const string DEFAULT = "";
+	
+		#endregion
+	}
+}
