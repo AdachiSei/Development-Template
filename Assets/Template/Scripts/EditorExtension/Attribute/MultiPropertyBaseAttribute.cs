30461d4cfd511c5e0bb09b88b473bf5a0e643551
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-04-19 21:59:57 +0900

    AttributeとDrawerの基底クラスを作った

---
+++ Assets/Template/Scripts/EditorExtension/Attribute/MultiPropertyBaseAttribute.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public abstract class MultiPropertyBaseAttribute : PropertyAttribute
+{
+    public MultiPropertyBaseAttribute[] Attributes;
+    public IAttributePropertyDrawer[] PropertyDrawers;
+
+    #if UNITY_EDITOR
+
+    public abstract void OnPreGUI(Rect position, SerializedProperty property);
+
+    public abstract void OnPostGUI(Rect position, SerializedProperty property, bool changed);
+
+    // アトリビュートのうち一つでもfalseだったらそのGUIは非表示になる
+    public virtual bool IsVisible(SerializedProperty property)
+    {
+        return true;
+    }
+
+    #endif
+}
