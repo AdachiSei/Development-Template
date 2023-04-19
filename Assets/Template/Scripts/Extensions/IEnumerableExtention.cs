068fefbe8775a550394533a17726bd76568e5640
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-04-19 21:56:05 +0900

    拡張メソッドを追加

---
+++ Assets/Template/Scripts/Extensions/IEnumerableExtention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Template.Extension
+{
+    /// <summary>
+    /// スクリプト
+    /// </summary>
+    public static class IEnumerableExtention
+    {
+        public static void Foreach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable) action(item);
+        }
+    }
+}
