d0b4df070304a42b1cf6cb80aa99b1ef023604f8
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-06-18 23:52:17 +0900

    AudioDataのボリューム設定を消した

--- Assets/Template/Scripts/Manager/Sound/AudioData/AudioData.cs
+++ Assets/Template/Scripts/Manager/Sound/AudioData/AudioData.cs
@@ -13,7 +13,6 @@ namespace Template.AudioData
         #region Properties
 
         public string NickName => _nickName;
-        public int Volume => _volume;
         public AudioClip AudioClip => _clip;
 
         #endregion
@@ -23,10 +22,6 @@ namespace Template.AudioData
         [SerializeField]
         [Header("名前")]
         private string _nickName;
-
-        [SerializeField]
-        [Header("音量")]
-        private int _volume = 1;
 
         [SerializeField]
         [Header("効果音")]
